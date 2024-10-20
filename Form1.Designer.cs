f4988f289393f49967b446e16c1559cd7f9dd013
sadeval <162736837+sadeval@users.noreply.github.com>
2024-10-20 12:46:16 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,51 @@
+﻿namespace GutenbergAuthorBookDownloader
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.textBoxAuthorName = new System.Windows.Forms.TextBox();
+            this.buttonDownload = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBoxAuthorName
+            // 
+            this.textBoxAuthorName.Location = new System.Drawing.Point(12, 12);
+            this.textBoxAuthorName.Size = new System.Drawing.Size(400, 22);
+            this.textBoxAuthorName.Text = "Введите имя и фамилию автора"; 
+            this.textBoxAuthorName.GotFocus += (s, e) => { if (textBoxAuthorName.Text == "Введите имя и фамилию автора") textBoxAuthorName.Text = ""; };
+            this.textBoxAuthorName.LostFocus += (s, e) => { if (string.IsNullOrWhiteSpace(textBoxAuthorName.Text)) textBoxAuthorName.Text = "Введите имя и фамилию автора"; };
+            // 
+            // buttonDownload
+            // 
+            this.buttonDownload.Location = new System.Drawing.Point(420, 10);
+            this.buttonDownload.Size = new System.Drawing.Size(75, 25);
+            this.buttonDownload.Text = "Скачать";
+            this.buttonDownload.Click += new System.EventHandler(this.buttonDownload_Click);
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(507, 50);
+            this.Controls.Add(this.buttonDownload);
+            this.Controls.Add(this.textBoxAuthorName);
+            this.Name = "Form1";
+            this.Text = "Скачивание книг автора из Гутенберга";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        private System.Windows.Forms.TextBox textBoxAuthorName;
+        private System.Windows.Forms.Button buttonDownload;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using HtmlAgilityPack;
+
+namespace GutenbergAuthorBookDownloader
+{
+    public partial class Form1 : Form
+    {
+        private const string SearchUrl = "https://www.gutenberg.org/ebooks/search/?query=";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void buttonDownload_Click(object sender, EventArgs e)
+        {
+            string authorName = textBoxAuthorName.Text.Trim();
+
+            if (string.IsNullOrEmpty(authorName))
+            {
+                MessageBox.Show("Пожалуйста, введите имя и фамилию автора.", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            try
+            {
+                var books = await GetBooksByAuthorAsync(authorName);
+                await DownloadBooksAsync(books);
+                MessageBox.Show("Скачивание завершено!", "Успех", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка: {ex.Message}", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private async Task<List<Book>> GetBooksByAuthorAsync(string authorName)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                var response = await client.GetStringAsync(SearchUrl + Uri.EscapeDataString(authorName));
+                var doc = new HtmlAgilityPack.HtmlDocument(); 
+                doc.LoadHtml(response);
+
+                var bookNodes = doc.DocumentNode.SelectNodes("//li[contains(@class, 'booklink')]");
+
+                if (bookNodes == null)
+                    throw new Exception("Не удалось найти книги для указанного автора.");
+
+                List<Book> books = new List<Book>();
+                foreach (var bookNode in bookNodes)
+                {
+                    var titleNode = bookNode.SelectSingleNode(".//h3/a");
+                    if (titleNode != null)
+                    {
+                        string title = titleNode.InnerText.Trim();
+                        string bookUrl = "https://www.gutenberg.org" + titleNode.GetAttributeValue("href", "");
+                        books.Add(new Book { Title = title, Url = bookUrl });
+                    }
+                }
+
+                return books;
+            }
+        }
+
+        private async Task DownloadBooksAsync(List<Book> books)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                string downloadFolder = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "GutenbergBooks");
+
+                if (!Directory.Exists(downloadFolder))
+                {
+                    Directory.CreateDirectory(downloadFolder);
+                }
+
+                foreach (var book in books)
+                {
+                    var response = await client.GetStringAsync(book.Url);
+                    var doc = new HtmlAgilityPack.HtmlDocument(); 
+                    doc.LoadHtml(response);
+
+                    var textNode = doc.DocumentNode.SelectSingleNode("//a[contains(@title, 'Plain Text')]");
+                    if (textNode != null)
+                    {
+                        string downloadUrl = "https://www.gutenberg.org" + textNode.GetAttributeValue("href", "");
+                        var bookContent = await client.GetStringAsync(downloadUrl);
+
+                        string safeTitle = Path.GetInvalidFileNameChars().Aggregate(book.Title, (current, c) => current.Replace(c.ToString(), ""));
+                        string filePath = Path.Combine(downloadFolder, $"{safeTitle}.txt");
+                        File.WriteAllText(filePath, bookContent);
+                    }
+                }
+            }
+        }
+
+    }
+
+    public class Book
+    {
+        public string Title { get; set; }
+        public string Url { get; set; }
+    }
+}
